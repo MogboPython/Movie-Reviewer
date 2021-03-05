d6122376162dc1150f7775265b12b6728da81f34
Onayade Mogboluwaga Tolulope <mogbo18@yahoo.com>
2021-03-06 00:04:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 #Configure MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'mogbo0108'
+app.config['MYSQL_PASSWORD'] = 'abcdefg'
 app.config['MYSQL_DB'] = 'myflaskapp'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
